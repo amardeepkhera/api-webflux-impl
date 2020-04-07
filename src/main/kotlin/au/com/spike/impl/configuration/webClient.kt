bfaa3ca996f41737b1474cf69a208978a3126333
s301047 <amardeep.singhkhera@iag.com.au>
2020-04-07 17:04:27 +1000

    initial setup

---
+++ src/main/kotlin/au/com/spike/impl/configuration/webClient.kt
@@ -0,0 +1,6 @@
+package au.com.spike.impl.configuration
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class WebClientConfig {}
---
+++ src/main/kotlin/au/com/spike/impl/web/MyController.kt
@@ -0,0 +1,7 @@
+package au.com.spike.impl.web
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MyController {
+}
